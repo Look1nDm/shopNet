a899092e455e85a371da050fe1821e3e482e462b
dimka <dimka411841994@gmail.com>
2023-01-31 12:29:12 +0300

    initial commit

---
+++ src/main/java/com/example/shopnet/ShopNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopNetApplication.class, args);
+    }
+
+}
